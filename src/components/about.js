f527127a7e0574c518475640ebd86093ce71b666
Felipe <felipe@fabricadecodigo.com>
2020-03-11 08:39:01 -0300

    section sobre

---
+++ src/components/about.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useStaticQuery, graphql } from "gatsby"
+
+const About = () => {
+    const data = useStaticQuery(graphql`
+    {
+      site {
+        siteMetadata {
+          aboutMe {
+            description
+            photoUrl
+            title
+          }
+          presentation {
+            name
+          }
+        }
+      }
+    }
+  `)
+    return (
+        <section id="sobre" className="section-portfolio-item bg-gray-100">
+            <h2 className="text-3xl font-bold uppercase text-center">Sobre Mim</h2>
+            <p className="text-center">
+                <img src={data.site.siteMetadata.aboutMe.photoUrl}
+                className="rounded-full border-8 border-white shadow-xl inline-block my-5"
+                alt={data.site.siteMetadata.presentation.name} title={data.site.siteMetadata.presentation.name} />
+            </p>
+            <h3 className="text-xl font-bold my-2 text-center">{data.site.siteMetadata.aboutMe.title}</h3>
+            <p dangerouslySetInnerHTML={ { __html: data.site.siteMetadata.aboutMe.description } }></p>
+        </section>
+    )
+}
+
+export default About
+
