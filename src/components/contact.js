577a937d2d1c4e330b4d1135443593736d9812d2
Felipe <felipe@fabricadecodigo.com>
2020-04-03 08:54:41 -0300

    form fix

--- src/components/contact.js
+++ src/components/contact.js
@@ -3,8 +3,11 @@ import Section from './section';
 
 export default () =>
     <Section id="contact" title="Contato" className="bg-gray-100">
-        <form name="contact" method="post" data-netlify="true">
+        <form name="contact" method="post" data-netlify="true" netlify-honeypot="bot-field">
             <input type="hidden" name="form-name" value="contact" />
+            <p class="hidden">
+                <label>Não preencha isso se você for humano: <input name="bot-field" /></label>
+            </p>
 
             <p>
                 <label>Name <input type="text" name="name" /></label>
@@ -13,7 +16,7 @@ export default () =>
                 <label>Email <input type="email" name="email" /></label>
             </p>
             <p>
-                <label>Mensagem <textarea name="message"  rows="5" /></label>
+                <label>Mensagem <textarea name="message" rows="5" /></label>
             </p>
             <p className="text-center">
                 <button type="submit" className="btn btn-primary">Enviar</button>
