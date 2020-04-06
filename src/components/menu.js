3a84e37fde65ae3fd6131470840fe0f6c2ffc62d
Felipe <felipe@fabricadecodigo.com>
2020-04-06 09:42:35 -0300

    ajustes no menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -5,8 +5,8 @@ export default () =>
     <div className="flex flex-wrap justify-center">
         <Link to="/" className="mx-2">Home</Link>
         <a href="#sobre" className="mx-2">Sobre</a>
-        <a href="sobre" className="mx-2">Experiências</a>
-        <a href="sobre" className="mx-2">Projetos</a>
-        <a href="sobre" className="mx-2">Skills</a>
-        <a href="sobre" className="mx-2">Contato</a>
+        <a href="#experiences" className="mx-2">Experiências</a>
+        <a href="#projects" className="mx-2">Projetos</a>
+        <a href="#skills" className="mx-2">Skills</a>
+        <a href="#contact" className="mx-2">Contato</a>
     </div>
